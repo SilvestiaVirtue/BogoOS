8447620531f556f530e4e218d259c454aa53a87c
SilvestiaVirtue <shkinyoo@gmail.com>
2026-01-10 22:26:57 +0400

    Create stdint.h

---
+++ src/lib/stdint.h
@@ -0,0 +1,10 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef int             int32_t;
+typedef unsigned int    size_t;
+
+#endif
