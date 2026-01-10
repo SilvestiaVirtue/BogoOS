4d81e492854f56cf5ab432f13dde7802fbf2dfd7
SilvestiaVirtue <shkinyoo@gmail.com>
2026-01-10 22:14:14 +0400

    Create remap.c

---
+++ src/kernel/remap.c
@@ -0,0 +1,23 @@
+#include "drivers/io.h"
+
+#define PIC1_COMMAND 0x20
+#define PIC1_DATA    0x21
+#define PIC2_COMMAND 0xA0
+#define PIC2_DATA    0xA1
+
+void pic_remap() {
+    outb(PIC1_COMMAND, 0x11);
+    outb(PIC2_COMMAND, 0x11);
+
+    outb(PIC1_DATA, 0x20);
+    outb(PIC2_DATA, 0x28);
+
+    outb(PIC1_DATA, 0x04);
+    outb(PIC2_DATA, 0x02);
+
+    outb(PIC1_DATA, 0x01);
+    outb(PIC2_DATA, 0x01);
+
+    outb(PIC1_DATA, 0xFD);
+    outb(PIC2_DATA, 0xFF);
+}
